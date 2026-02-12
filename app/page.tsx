3b6e77a12cd0e02ceaae1c2e15572722d48f9b55
Athibet Bootklang <athibet.b@gmail.com>
2026-02-12 17:29:00 +0700

    add: 1 for checkpoint

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-      <div>Img</div>
+      <div>Img1</div>
       <div className="flex flex-col justify-center items-center">
         <p>Log into Instagram</p>
         <div>1</div>
